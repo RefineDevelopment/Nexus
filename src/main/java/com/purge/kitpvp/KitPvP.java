ea3df7a8885681cc2f5fc80c277e99891b182d7d
veltus5184 <nathakorn.jumderm@gmail.com>
2021-06-08 17:59:34 +0700

    [+] Added Cuboid.java util

--- src/main/java/com/purge/kitpvp/KitPvP.java
+++ src/main/java/com/purge/kitpvp/KitPvP.java
@@ -1,8 +1,13 @@
 package com.purge.kitpvp;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.purge.kitpvp.managers.KitManager;
 import com.purge.kitpvp.managers.ProfileManager;
 import lombok.Getter;
+import org.bson.Document;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.concurrent.Executor;
@@ -20,6 +25,10 @@ public class KitPvP extends JavaPlugin {
     @Getter public Executor mainThread;
     @Getter public Executor mongoThread;
     @Getter public Executor taskThread;
+
+    @Getter private MongoClient mongoClient;
+    @Getter private MongoDatabase mongoDatabase;
+    @Getter private static MongoCollection<Document> profiles;
 
     @Override
     public void onLoad() {
@@ -41,10 +50,20 @@ public class KitPvP extends JavaPlugin {
 
 
         });
+
+        mongoThread.execute(this::connect);
     }
 
     @Override
     public void onDisable() {
         instance = null;
+    }
+
+    public void connect() {
+        MongoClientURI mongoClientURI = new MongoClientURI(getConfig().getString("mongo-uri"));
+        mongoClient = new MongoClient(mongoClientURI);
+        mongoDatabase = mongoClient.getDatabase("kitpvp");
+        profiles = mongoDatabase.getCollection("profiles");
+
     }
 }
---
+++ src/main/java/com/purge/kitpvp/utils/Cuboid.java
@@ -0,0 +1,27 @@
+package com.purge.kitpvp.utils;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class Cuboid {
+
+    private final Vector minV;
+    private final Vector maxV;
+
+    public Cuboid(Location min, Location max) {
+        double xPos1 = Math.min(min.getX(), max.getX());
+        double yPos1 = Math.min(min.getY(), max.getY());
+        double zPos1 = Math.min(min.getZ(), max.getZ());
+
+        double xPos2 = Math.max(min.getX(), max.getX());
+        double yPos2 = Math.max(min.getY(), max.getY());
+        double zPos2 = Math.max(min.getZ(), max.getZ());
+
+        this.minV = new Vector(xPos1, yPos1, zPos1);
+        this.maxV = new Vector(xPos2, yPos2, zPos2);
+    }
+
+    public boolean containsLocation(Location location) {
+        return location.toVector().isInAABB(minV, maxV);
+    }
+}
